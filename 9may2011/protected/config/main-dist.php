32b66830d46a165a284b9257cbb2ffc0c0c6fe36
merge <to.merge@gmail.com>
2011-04-21 11:30:29 +0600

    directory refactor according http://www.yiiframework.com/wiki/155/the-directory-structure-of-the-yii-project-site/

--- 9may2011/protected/config/main-dist.php
+++ 9may2011/protected/config/main-dist.php
@@ -34,6 +34,7 @@ return array(
 	// application components
 	'components'=>array(
 		'user'=>array(
+			'class' => 'WebUser',
 			// enable cookie-based authentication
 			'allowAutoLogin'=>true,
 		),
---
+++ 9may2011/protected/www/index-dist.php
@@ -0,0 +1,12 @@
+<?php
+
+// remove the following lines when in production mode
+defined('YII_DEBUG') or define('YII_DEBUG',false);
+// specify how many levels of call stack should be shown in each log message
+defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
+
+require_once(dirname(__FILE__).'/../../framework/yii.php');
+$configDist=require(dirname(__FILE__).'/../config/main-dist.php');
+$config=require(dirname(__FILE__).'/../config/main.php');
+$config=CMap::mergeArray($configDist, $config);
+Yii::createWebApplication($config)->run();
