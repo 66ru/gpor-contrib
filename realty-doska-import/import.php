ad115e2672b527caec295b49b21b7719ba737f32
Кузнецов Алексей <vamereh@gmail.com>
2011-06-09 15:07:47 +0600

    OSP-1488: fix

--- realty-doska-import/import.php
+++ realty-doska-import/import.php
@@ -2,6 +2,8 @@
 
 require ('../_lib/xmlrpc-3.0.0.beta/xmlrpc.inc');
 $params = require('config.php');
+if (!$params)
+	die('Error: config.php not found.');
 
 //TODO: Не забыть убрать
 $kkey = isset($params['apiKey']) ? $params['apiKey'] : false;
@@ -14,39 +16,39 @@ if (!$apiUrl)
 //Получение счетчиков с доски по недвижимости жилых поещений и их парсинг
 $realty_live_counters_data = @file_get_contents('http://66.ru/ajax/realty_live_counters.json');
 if ($realty_live_counters_data===false){
-	echo('cant get http://66.ru/ajax/realty_live_counters.json \n');
+	echo("cant get http://66.ru/ajax/realty_live_counters.json \n");
 	$realty_live_counters_data = "";
 }
 else{
 	$realty_live_counters_arr = @json_decode($realty_live_counters_data, true);
 	if (!is_array($realty_live_counters_arr))
-		echo('cant parse http://66.ru/ajax/realty_live_counters.json \n');
+		echo("cant parse http://66.ru/ajax/realty_live_counters.json \n");
 	$realty_live_counters_arr = null;
 }
 
 //Получение объявлений с доски по недвижимости и их парсинг
 $realty_live_msg_data = @file_get_contents('http://66.ru/ajax/latest50_live.json');
 if ($realty_live_msg_data===false){
-	echo('cant get http://66.ru/ajax/latest50_live.json \n');
+	echo("cant get http://66.ru/ajax/latest50_live.json \n");
 	$realty_live_msg_data = "";
 }
 else{
 $realty_live_msg_arr = @json_decode($realty_live_msg_data, true);
 	if (!is_array($realty_live_msg_arr))
-		echo('cant parse http://66.ru/ajax/latest50_live.json \n');
+		echo("cant parse http://66.ru/ajax/latest50_live.json \n");
 	$realty_live_msg_arr = null;
 }
 
 //Получение счетчиков с доски по недвижимости нежилых помещений и их парсинг
 $realty_com_counters_data = @file_get_contents('http://66.ru/ajax/realty_com_counters.json');
 if ($realty_com_counters_data===false){
-	echo('cant get http://66.ru/ajax/realty_com_counters.json \n');
+	echo("cant get http://66.ru/ajax/realty_com_counters.json \n");
 	$realty_com_counters_data = "";
 }
 else{
 	$realty_com_counters_arr = @json_decode($realty_com_counters_data, true);
 	if (!is_array($realty_com_counters_arr))
-		echo('cant parse http://66.ru/ajax/realty_com_counters.json \n');
+		echo("cant parse http://66.ru/ajax/realty_com_counters.json \n");
 	$realty_com_counters_arr = null;
 }
 //Отправка данных
