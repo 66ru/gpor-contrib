3f8a83b02eabfb8fc2ebe471be52be07c5927c8f
stepanoff <stenlex@gmail.com>
2011-09-19 10:56:43 +0600

    OSP-2198 final

--- rabota66-scripts/cron/gporExport.php
+++ rabota66-scripts/cron/gporExport.php
@@ -5,8 +5,7 @@
 	include_once( ROOT.'/lib/gporImport.class.php' );
 	
 	$xmlRpcConfig = include( ROOT.'/config/xmlrpc.conf.php' );
-	
-	
+
 	$kkey = isset($xmlRpcConfig['apiKey']) ? $xmlRpcConfig['apiKey'] : false;
 	$apiUrl = isset($xmlRpcConfig['apiUrl']) ? $xmlRpcConfig['apiUrl'] : false;
 	if (!$kkey)
@@ -14,45 +13,25 @@
 	if (!$apiUrl)
 		die('Error. "apiUrl" not found in config.php');
 		
-	$lastLaunchFile = ROOT.'/cron/gporImport.txt';
-
+	$lastLaunchFile = ROOT.'/cron/gporExport.txt';
 	$lastLaunchTime = 0;
 	if (file_exists($lastLaunchFile))
 		$lastLaunchTime = file_get_contents($lastLaunchFile);
 
-	$lockFile = ROOT.'/cron/gporImport.lock';
+	$lockFile = ROOT.'/cron/gporExport.lock';
 	if (file_exists($lockFile))
 		return;
 	file_put_contents($lockFile, $lastLaunchTime);
-
 	
 	$import = new gporImport();
 	$import->setLastLaunchTime($lastLaunchTime);
 	$import->apiUrl = $apiUrl;
 	$import->apiKey = $kkey;
-	$import->limit = 10;
 
-	$lastLaunchTime = time();
-	$import->setLastId(1);
-	$res = $import->importCompanies();
-	while ($res)
-	{
-		$res = $import->importCompanies();
-		$import->clearLog();
-	}
-	$import->clearLog();
-
-	$import->setLastId(1);
-	$res = $import->importVacancies();
-	while ($res)
-	{
-		$res = $import->importVacancies();
-		$import->clearLog();
-	}
-
-	$import->hideVacancies();
+	$import->exportResponses();
 	$import->clearLog();
 
 	file_put_contents($lastLaunchFile, $lastLaunchTime);
 	unlink ($lockFile);
+
 ?>
