f230ceb28ea3e2027e51ebcc93ef6598bfa050b6
Alexander M <a.manichev@gmail.com>
2011-12-16 17:06:26 +0600

    re #393 add round on temperature

--- weather-current-meteo_hmn/ekburg.php
+++ weather-current-meteo_hmn/ekburg.php
@@ -4,7 +4,7 @@ $ekburgWeather = file_get_contents('http://www.ekburg.ru/.out/weatherSite/weathe
 if (!empty($ekburgWeather)) {
 	$ekburgWeather = json_decode($ekburgWeather, true);
 	if (isset($ekburgWeather['weather']['deg']) && is_numeric($ekburgWeather['weather']['deg']))
-		return $ekburgWeather['weather']['deg'];
+		return round($ekburgWeather['weather']['deg']);
 }
 
 return false;
