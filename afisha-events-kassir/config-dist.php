5f7ed0269c8a4bc48713b5b4a4c408b9ff35f69a
da <darkair2@gmail.com>
2012-10-03 13:20:57 +0600

    #afisha-events-kassir

--- afisha-events-kassir/config-dist.php
+++ afisha-events-kassir/config-dist.php
@@ -1,19 +1,3 @@
 <?php
-$apiUrl       = 'http://66.bazilio/api/';
-$apiKey       = 'телега, например';
-$accessPlaces = array(
-	'Космос'                       => true,
-	'Знамя'                        => true,
-	'Заря'                         => true,
-	'Дом кино'                     => true,
-	'Синема Парк Старлайт'         => true,
-	'Киноплекс на Халтурина'       => true,
-	'Киноплекс на Сулимова'        => true,
-	'Prada 3D'                     => true,
-	'3D Мегаполис'                 => true,
-	'Екатерининский'               => true,
-	'Салют'                        => true,
-	'Титаник-синема'               => true,
-	'Юго-Западный'                 => true,
-	'Колизей'                      => true,
-);
+$apiUrl       = 'http://api.66.localhost/';
+$apiKey       = '386749b96b1ceb8001c15cc25e8d358b';
