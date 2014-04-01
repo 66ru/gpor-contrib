88cf9e1ab0ccd03fdb22a7754a5e91d88f63041a
deface90 <deface06@yandex.ru>
2014-04-01 17:17:48 +0600

    kinohod import improvements mediasite/gpor#3011

--- afisha-cinema-kinohod/config-dist.php
+++ afisha-cinema-kinohod/config-dist.php
@@ -8,6 +8,7 @@ return array (
 	'kApiUrl' 	  => '', // https://kinohod.ru/api/rest/partner/v1/
 	'kApiKey'	  => '',
 	'kPurchaseUrl'=> '', // http://kinohod.ru/api/widget/seance/
+    'kPurchaseUrlMobile' => '',
 	'kClientApiKey' => '',
 	'kCityId' => false, // see https://kinohod.ru/api/rest/partner/v1/cities/?apikey={ kApiKey }
 
