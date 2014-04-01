18d25a25a4803a904ed6323a5ba3c0f036e40955
deface90 <deface06@yandex.ru>
2014-04-01 17:21:25 +0600

    -

--- afisha-cinema-kinohod/config-dist.php
+++ afisha-cinema-kinohod/config-dist.php
@@ -2,15 +2,15 @@
 
 return array (
 
-	'apiUrl'       => '', // http://api.domain.ws
-	'apiKey'       => '',
+    'apiUrl'       => '', // http://api.domain.ws
+    'apiKey'       => '',
 
-	'kApiUrl' 	  => '', // https://kinohod.ru/api/rest/partner/v1/
-	'kApiKey'	  => '',
-	'kPurchaseUrl'=> '', // http://kinohod.ru/api/widget/seance/
+    'kApiUrl'     => '', // https://kinohod.ru/api/rest/partner/v1/
+    'kApiKey'     => '',
+    'kPurchaseUrl'=> '', // http://kinohod.ru/api/widget/seance/
     'kPurchaseUrlMobile' => '',
-	'kClientApiKey' => '',
-	'kCityId' => false, // see https://kinohod.ru/api/rest/partner/v1/cities/?apikey={ kApiKey }
+    'kClientApiKey' => '',
+    'kCityId' => false, // see https://kinohod.ru/api/rest/partner/v1/cities/?apikey={ kApiKey }
 
-	'debug' 	  => false,
+    'debug'       => false,
 );
